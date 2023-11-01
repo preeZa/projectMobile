13fb4bb746e2fb444c598574a6a4bfda84405e3d
Nashi <115577029+NashiZz@users.noreply.github.com>
2023-11-01 21:31:55 +0700

    1

--- lib/screens/home/page/shirt_page.dart
+++ lib/screens/home/page/shirt_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:projectMobile/main.dart';
 
 import '../../../Services.dart';
 import '../../../constants.dart';
@@ -47,8 +48,11 @@ class _ShirtpageState extends State<Shirtpage> {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()));
+          },
+          icon: SvgPicture.asset("assets/icons/back.svg"),
         ),
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -97,87 +101,87 @@ class _ShirtpageState extends State<Shirtpage> {
                         ),
                       ),
                       for (int i = 0; i < products!.products.length; i++)
-                         if (products!.products[i].name == something)
-                        SingleChildScrollView(
-                          physics: const BouncingScrollPhysics(
-                              parent: AlwaysScrollableScrollPhysics()),
-                          scrollDirection: Axis.horizontal,
-                          child: Column(children: [
-                            Container(
-                              margin: EdgeInsets.only(
-                                  top: 30, bottom: 30, left: 10, right: 10),
-                              // padding: EdgeInsets.symmetric(horizontal: 20),
-                              height: 350,
-                              width: 320,
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: NetworkImage(
-                                        products!.products[i].image),
-                                    fit: BoxFit.cover),
-                                borderRadius: BorderRadius.circular(20),
+                        if (products!.products[i].name == something)
+                          SingleChildScrollView(
+                            physics: const BouncingScrollPhysics(
+                                parent: AlwaysScrollableScrollPhysics()),
+                            scrollDirection: Axis.horizontal,
+                            child: Column(children: [
+                              Container(
+                                margin: EdgeInsets.only(
+                                    top: 30, bottom: 30, left: 10, right: 10),
+                                // padding: EdgeInsets.symmetric(horizontal: 20),
+                                height: 350,
+                                width: 320,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          products!.products[i].image),
+                                      fit: BoxFit.cover),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
                               ),
-                            ),
-                            SizedBox(
-                              child: Row(
-                                children: [
-                                  Text("Shirt",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline6!
-                                          .copyWith(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black87)),
-                                  const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: defaultPadding),
-                                  ),
-                                  Text("250 Bath",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline6!
-                                          .copyWith(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.deepOrange,
-                                              fontSize: 16)),
-                                  const Padding(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: defaultPadding),
-                                  ),
-                                  FloatingActionButton.extended(
-                                    label: Text('Add Cart'),
-                                    backgroundColor: Colors.brown,
-                                    icon: Icon(
-                                      Icons.shopping_bag_sharp,
-                                      size: 24.0,
+                              SizedBox(
+                                child: Row(
+                                  children: [
+                                    Text("Shirt",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline6!
+                                            .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black87)),
+                                    const Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: defaultPadding),
                                     ),
-                                    onPressed: () {},
-                                  ),
-                                ],
-                              ),
-                            )
-                          ]
-                              // List.generate(
-                              //   products!.products.length,
-                              //   (index) => Padding(
-                              //     padding: const EdgeInsets.only(right: defaultPadding),
-                              //     child: ProductCard(
-                              //       title: "",
-                              //       image: products!.products[index].image,
-                              //       price: products!.products[index].price.toInt(),
-                              //       bgColor: Color.fromARGB(255, 255, 255, 255),
-                              //       press: () {
-                              //         // Navigator.push(
-                              //         //     context,
-                              //         //     MaterialPageRoute(
-                              //         //       builder: (context) =>
-                              //         //           DetailsScreen(product: demo_product[index]),
-                              //         //     ));
-                              //       },
-                              //     ),
-                              //   ),
-                              // ),
-                              ),
-                        )
+                                    Text("250 Bath",
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .headline6!
+                                            .copyWith(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.deepOrange,
+                                                fontSize: 16)),
+                                    const Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: defaultPadding),
+                                    ),
+                                    FloatingActionButton.extended(
+                                      label: Text('Add Cart'),
+                                      backgroundColor: Colors.brown,
+                                      icon: Icon(
+                                        Icons.shopping_bag_sharp,
+                                        size: 24.0,
+                                      ),
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ]
+                                // List.generate(
+                                //   products!.products.length,
+                                //   (index) => Padding(
+                                //     padding: const EdgeInsets.only(right: defaultPadding),
+                                //     child: ProductCard(
+                                //       title: "",
+                                //       image: products!.products[index].image,
+                                //       price: products!.products[index].price.toInt(),
+                                //       bgColor: Color.fromARGB(255, 255, 255, 255),
+                                //       press: () {
+                                //         // Navigator.push(
+                                //         //     context,
+                                //         //     MaterialPageRoute(
+                                //         //       builder: (context) =>
+                                //         //           DetailsScreen(product: demo_product[index]),
+                                //         //     ));
+                                //       },
+                                //     ),
+                                //   ),
+                                // ),
+                                ),
+                          )
                     ],
                   ),
 
